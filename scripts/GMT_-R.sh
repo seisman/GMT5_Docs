fcb54894e76ca694762cc16a49393dfe504decc4
Dongdong Tian <seisman.info@gmail.com>
2015-05-02 23:19:09 +0800

    fix unit bugs in script

--- scripts/GMT_-R.sh
+++ scripts/GMT_-R.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#	$Id: GMT_-R.sh 11490 2013-05-16 06:26:21Z pwessel $
+#
 gmt gmtset MAP_FRAME_TYPE PLAIN FONT_ANNOT_PRIMARY 8p,Helvetica MAP_TICK_LENGTH_PRIMARY 0.05i \
 	PS_CHAR_ENCODING ISOLatin1+
 gmt pscoast -R-90/-70/18/35.819 -JM2i -P -Dl -Glightbrown -Wthinnest -Ba10g5 -BWsEN -K > GMT_-R.ps
-gmt pstext -R0/2/-0.5/2 -Jx1i -O -K -N -Y-0.5 -F+f9p,Helvetica-Oblique+jCT << EOF >> GMT_-R.ps
+gmt pstext -R0/2/-0.5/2 -Jx1i -O -K -N -Y-0.5i -F+f9p,Helvetica-Oblique+jCT << EOF >> GMT_-R.ps
 1	-0.375	@%0%a)@%% @%1%\035R@%%xmin/xmax/ymin/ymax
 EOF
 gmt psxy -R -J -O -K -N -Sc0.1 -Wthinner << EOF >> GMT_-R.ps
@@ -26,8 +28,8 @@ gmt psxy -R -J -O -K -N -Wthinner << EOF >> GMT_-R.ps
 1.05	-0.35
 EOF
 #
-gmt pscoast -R-90/20/-65.5327/29.4248r -JOc280/20/22/69/2 -O -Dl -Glightbrown -Wthinnest -Ba10g5 -BWsEN -K -X2.75 -Y0.5 >> GMT_-R.ps
-gmt pstext -R0/2/-0.5/2 -Jx1i -O -K -N -Y-0.5 -F+f9p,Helvetica-Oblique+jCT << EOF >> GMT_-R.ps
+gmt pscoast -R-90/20/-65.5327/29.4248r -JOc280/20/22/69/2i -O -Dl -Glightbrown -Wthinnest -Ba10g5 -BWsEN -K -X2.75i -Y0.5i >> GMT_-R.ps
+gmt pstext -R0/2/-0.5/2 -Jx1i -O -K -N -Y-0.5i -F+f9p,Helvetica-Oblique+jCT << EOF >> GMT_-R.ps
 1	-0.375	@%0%b)@%% @%1%\035R@%%xlleft/ylleft/xuright/yuright @%1%r@%%
 EOF
 gmt psxy -R -J -O -K -N -Sc0.1 -Wthinner << EOF >> GMT_-R.ps
