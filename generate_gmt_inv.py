12ef5489b8491143e48e5d10b2c98d33406966fe
Dongdong Tian <seisman.info@gmail.com>
2015-11-04 16:11:21 +0800

    完善inv文件

--- generate_gmt_inv.py
+++ generate_gmt_inv.py
@@ -14,10 +14,51 @@ inventory_header = '''\
 # The remainder of this file is compressed with zlib.
 '''.encode('utf-8')
 
-modules = ['pscoast', 'gmt', 'gmtget', 'gmtset', 'gmtdefaults', 'gmtlogo',
-           'gmt5syntax', 'isogmt', 'psbasemap', 'pscoast', 'grdpaste',
-           'grdcut', 'pstext', 'ps2raster', 'grdraster', 'grdedit', 'psclip',
-           'psxy', 'kml2gmt', 'gmtinfo', 'grd2xyz'
+modules = ['gmt', 'gmtlogo', 'gmt5syntax', 'isogmt',
+
+           'blockmean', 'blockmedian', 'blockmode', 'filter1d', 'fitcircle',
+           'gmt2kml', 'gmtconnect', 'gmtconvert', 'gmtdefaults', 'gmtget',
+           'gmtinfo', 'gmtmath', 'gmtselect', 'gmtset', 'gmtsimplify',
+           'gmtspatial', 'gmtswitch', 'gmtvector',
+
+           'grd2cpt', 'grd2rgb', 'grd2xyz', 'grdblend', 'grdclip',
+           'grdcontour', 'grdcut', 'grdedit', 'grdfft', 'grdfilter',
+           'grdgradient', 'grdhisteq', 'grdimage', 'grdinfo',
+           'grdlandmask', 'grdmask', 'grdmath', 'grdpaste', 'grdproject',
+           'grdraster', 'grdreformat', 'grdsample', 'grdtrack', 'grdtrend',
+           'grdvector', 'grdview', 'grdvolume',
+
+           'greenspline', 'kml2gmt', 'makecpt', 'mapproject', 'nearneighbor',
+
+           'ps2raster', 'psbasemap', 'psclip', 'pscoast', 'pstext',
+           'psxy', 'project', 'pscontour', 'pshistogram', 'psimage',
+           'pslegend', 'psmask', 'psrose', 'psscale', 'pswiggle', 'psxyz',
+
+           'sample1d', 'spectrum1d', 'sph2grd', 'sphdistance',
+           'sphinterpolate', 'sphtriangulate', 'splitxyz', 'triangulate',
+           'xyz2grd',
+
+           'gshhg',
+
+           'img2google', 'img2grd',
+
+           'pscoupe', 'psmeca', 'pspolar', 'psvelo',
+
+           'mgd77info', 'mgd77magref', 'mgd77path', 'mgd77track',
+           'mgd77convert', 'mgd77list', 'mgd77manage', 'mgd77sniffer',
+
+           'dimfilter',
+
+           'gmtgravmag3d', 'gravfft', 'grdgravmag3d', 'grdredpol', 'grdseamount',
+
+           'pssegy', 'pssegyz', 'segy2grd',
+
+           'backtracker', 'grdpmodeler', 'grdspotter', 'originator',
+           'grdrotater', 'hotspotter', 'rotconverter',
+
+           'x2sys_cross', 'x2sys_init', 'x2sys_put', 'x2sys_datalist',
+           'x2sys_list', 'x2sys_report', 'x2sys_binlist', 'x2sys_get',
+           'x2sys_merge', 'x2sys_solve',
            ]
 
 inventory_payload = ''.join(['{0} std:label -1 {0}.html {0}\n'.format(i) for i in modules]).encode('utf-8')
